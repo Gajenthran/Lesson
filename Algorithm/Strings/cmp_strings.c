2e01f72dc7b8d18c0f09fb50be62c685ed5daeb3
Panchalingamoorthy Gajenthran <p.gajenthran@gmail.com>
2018-11-05 20:01:18 +0100

    comparer chaine

---
+++ Algorithm/Strings/cmp_strings.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int comparer_chaine(char ** strs, int n, char * str) {
+    int i;
+    for(i = 0; i < n; i++) {
+        if(strcmp(strs[i], str) == 0)
+            return 1;
+    }
+    return 0;
+}
