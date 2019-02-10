3c45b72565b319fb678b13dca77a78ec9c635822
Panchalingamoorthy Gajenthran <p.gajenthran@gmail.com>
2019-02-11 00:11:34 +0100

    MPL: sobel filter

--- mpl/td2/td2-image/td2-filtrage-thread.cpp
+++ mpl/td2/td2-image/td2-filtrage-thread.cpp
@@ -3,20 +3,57 @@
 #include <future>
 #include <iostream>
 #include <random>
+#include <cmath>
 #include "td2-gsimage.hpp"
 #include <benchmark/benchmark.h>
 
-char const* img_src  = "img/moutain.jpg";
+char const* img_src  = "img/maison.jpg";
 char const* img_dest = "img/moutain_filter.jpg";
 
 bool filtre(GSImage::View& view) {
   float filtered_row[view.w];
   for(int r = view.y; r < view.h; ++r) {
-    float* row = view.operator[](r-view.y); // (view.image.pixels() + view.y) + r * view.w;
-    for(int c = view.x; c < view.w - 1; ++c) {
+    float* row = view.getPixel(r-view.y, 0); // (view.image.pixels() + view.y) + r * view.w;
+    for(int c = view.x; c < view.w; ++c) {
         filtered_row[c] = (row[c + 1] - row[c - 1]) / 2 + 128;
     }
     std::copy(filtered_row, filtered_row + view.w, row);
+  }
+  return true;
+}
+
+bool sobelFiltre(GSImage::View& view) {
+  int const gx[3][3] = {
+    {-1, 0, 1},
+    {-2, 0, 2},
+    {-1, 0, 1}
+  };
+
+  int const gy[3][3] = {
+    {-1, -2, -1},
+    {0, 0, 0},
+    {1, 2, 1}
+  };
+
+  float sx, sy, s;
+  for(int r = 0; r < view.h; ++r) {
+    float* row = view.operator[](r);
+    for(int c = 0; c < view.w; ++c) {
+      sx = sy = 0.0f;
+      for(int i = -1; i <= 1; i++) {
+        sx += row[c - view.image.w() + i] * (float)gx[0][i+1];
+        sx += row[c + i] * gx[1][i+1];
+        sx += row[c + view.image.w() + i] * (float)gx[2][i+1];
+
+        sy += row[c - view.image.w() + i] * (float)gy[0][i+1];
+        sy += row[c + i] * gy[1][i+1];
+        sy += row[c + view.image.w() + i] * (float)gy[2][i+1];
+      }
+
+      s = sqrt(sx * sx + sy * sy);// std::abs(sx) + std::abs(sy);
+      row[c] = s > 0.8f ? 0.0f : s < 0.0f ? 1.0f : s; 
+      // std::cout << s << "\n";
+    }
   }
   return true;
 }
@@ -30,9 +67,10 @@ bool x = false;
 
 void filtre_single_thread(benchmark::State& state) {
   GSImage image(img_src);
-  GSImage::View view = image.view(0, 0, image.w(), image.h());
+  GSImage::View view = image.view(0, 0, image.w(), image.h()); // image.w(), image.h());
   for(auto _ : state) {
-    x = filtre(view);
+    // x = filtre(view);
+    // x = sobelFiltre(view);
     break;
   }
   image.save_png(img_dest);
@@ -56,6 +94,6 @@ void filtre_multithread(benchmark::State& state) {
   image.save_png(img_dest);
 }
 
-// BENCHMARK(filtre_single_thread)->Arg(2);
-BENCHMARK(filtre_multithread)->Arg(2)->Arg(3)->Arg(4)->Arg(5)->Arg(6)->Arg(7)->Arg(8);
+BENCHMARK(filtre_single_thread)->Arg(2);
+// BENCHMARK(filtre_multithread)->Arg(2)->Arg(3)->Arg(4)->Arg(5)->Arg(6)->Arg(7)->Arg(8);
 BENCHMARK_MAIN();
--- mpl/td2/td2-image/td2-filtrage.cpp
+++ mpl/td2/td2-image/td2-filtrage.cpp
@@ -1,6 +1,7 @@
 // filtrage.cpp
 
 #include <iostream>
+#include <cmath>
 #include "td2-gsimage.hpp"
 
 
@@ -15,6 +16,41 @@ void filtre(GSImage& image) {
   }
 }
 
+bool sobelFiltre(GSImage& image) {
+  float const gx[3][3] = {
+    {-1.0, 0.0, 1.0},
+    {-2.0, 0.0, 2.0},
+    {-1.0, 0.0, 1.0}  
+  };
+
+  float const gy[3][3] = {
+    {-1.0, -2.0, -1.0},
+    {0.0, 0.0, 0.0},
+    {1.0, 2.0, 1.0}
+  };
+
+  float sx, sy, s;
+  for(int r = 0; r < image.h(); ++r) {
+    float* row = image.pixels() + r * image.w();
+    for(int c = 0; c < image.w() - 1; ++c) {
+      sx = sy = 0.0f;
+      for(int i = -1; i <= 1; i++) {
+        sx += row[c - image.w() + i] * gx[0][i+1];
+        sx += row[c + i] * gx[1][i+1];
+        sx += row[c + image.w() + i] * gx[2][i+1];
+
+        sy += row[c - image.w() + i] * gy[0][i+1];
+        sy += row[c + i] * gy[1][i+1];
+        sy += row[c + image.w() + i] * gy[2][i+1];
+      }
+
+      s = sqrt(sx * sx + sy * sy);
+      row[c] = s > 1.0f ? 0.0f : s < 0.0f ? 1.0f : s; 
+    }
+  }
+  return true;
+}
+
 void on_error(char const* executable) {
   std::cout << "Usage: " << executable << " <source_file.png> <target_file.png>\n";
   exit(1);
@@ -25,7 +61,7 @@ int main(int argc, char const* argv[]) {
     on_error(argv[0]);
 
   GSImage image(argv[1]);
-  filtre(image);
+  sobelFiltre(image);
   image.save_png(argv[2]);
 
   return 0;
