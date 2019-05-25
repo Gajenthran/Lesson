cf8a208218f778a0c7a170116a1724db9006dba5
Panchalingamoorthy Gajenthran <p.gajenthran@gmail.com>
2019-05-25 16:53:29 +0200

    IDL: chat - qui et ou

--- idl/chatbot/bots/super/sets/pp.py
+++ idl/chatbot/bots/super/sets/pp.py
@@ -2,10 +2,15 @@ import os
 
 f = open("job.txt", "r")
 d = ""
+
+
 for x in f:
   d += x.lower();
-		
+
+d = d.split("\n");
+d = sorted(d);
+d = "\n".join(d);
+print(d)
+
 with open("job.txt", 'w') as file:
 	file.write(d)
-
-
